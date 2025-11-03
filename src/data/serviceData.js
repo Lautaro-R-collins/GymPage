47de6238b5d8965e0f4bb64fe41a537aba968488
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-03 17:16:00 -0300

    add data service component

---
+++ src/data/serviceData.js
@@ -0,0 +1,27 @@
+export const services = [
+  {
+    title: "Weight Training",
+    description: "Build muscle and strength with guided weight programs.",
+  },
+  {
+    title: "Powerlifting",
+    description:
+      "Improve strength and power with specialized lifting techniques.",
+  },
+  {
+    title: "HIIT",
+    description: "High intensity workouts to push your limits.",
+  },
+  {
+    title: "Cardio",
+    description: "Boost endurance and heart health with cardio routines.",
+  },
+  {
+    title: "Personal Training",
+    description: "One-on-one coaching with certified trainers.",
+  },
+  {
+    title: "Boodybuilding",
+    description: "One-on-one coaching with certified trainers.",
+  },
+];
