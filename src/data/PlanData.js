129dbc87a6290e2c9b0027c3eec9f4b397a98bc5
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-03 17:16:10 -0300

    add planData component

---
+++ src/data/PlanData.js
@@ -0,0 +1,30 @@
+
+export const plans = [
+  {
+    title: "Basic",
+    price: "$29/mo",
+    features: [
+      "Access to personalized training",
+      "1 group online session/week",
+      "Basic nutrition tips"
+    ]
+  },
+  {
+    title: "Standard",
+    price: "$49/mo",
+    features: [
+      "All Basic features",
+      "3 online group sessions/week",
+      "Custom meal suggestions"
+    ]
+  },
+  {
+    title: "Premium",
+    price: "$79/mo",
+    features: [
+      "Unlimited online sessions",
+      "Full personalized nutrition plan",
+      "Progress tracking and coaching"
+    ]
+  }
+];
