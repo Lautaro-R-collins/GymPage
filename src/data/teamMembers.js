2a3c02cb800a26402101a43a6bf0e7fffa4c8616
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-03 19:52:59 -0300

    add - teamMembersData

---
+++ src/data/teamMembers.js
@@ -0,0 +1,22 @@
+export const teamMembers = [
+  {
+    name: "Alice Johnson",
+    role: "Nutrition Specialist",
+    image: "https://images.pexels.com/photos/414029/pexels-photo-414029.jpeg?auto=compress&cs=tinysrgb&w=400",
+    description: "Expert in personalized nutrition plans, dietary coaching, and meal optimization to enhance performance and health."
+  },
+
+  {
+    name: "Daniel Carter",
+    role: "Functional Training Coach",
+    image: "https://hips.hearstapps.com/hmg-prod/images/mh-trainer-2-1533576998.png",
+    description: "Specializes in functional training, improving mobility, core stability, and overall body mechanics for daily performance."
+  },
+
+  {
+    name: "Emma Wilson",
+    role: "Strength & Conditioning Coach",
+    image: "https://images.pexels.com/photos/416778/pexels-photo-416778.jpeg?auto=compress&cs=tinysrgb&w=400",
+    description: "Focuses on strength training, muscle development, and athletic performance through scientifically-backed programs."
+  },
+];
